2a7b1065f46ba9212ce3fecf8005d90d7e48b24a
sj-williams <steve.williams@digital.justice.gov.uk>
2022-10-11 13:10:04 +0100

    add produce handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sj-williams/proglog
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- internal/server/http.go
+++ internal/server/http.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -14,5 +15,51 @@ func NewHTTPServer(addr string) *http.Server {
 	return &http.Server{
 		Addr:    addr,
 		Handler: r,
+	}
+}
+
+type httpServer struct {
+	Log *Log
+}
+
+func newHTTPServer() *httpServer {
+	return &httpServer{
+		Log: NewLog(),
+	}
+}
+
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}
+
+func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
+	var req ProduceRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	off, err := s.Log.Append(req.Record)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res := ProduceResponse{Offset: off}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
